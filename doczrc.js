e461e2849821deae562b0834cd6f9e724d15b8bb
supportacems <54831473+supportacems@users.noreply.github.com>
2021-08-19 12:46:35 +0700

    Update doczrc.js

--- doczrc.js
+++ doczrc.js
@@ -4,6 +4,5 @@ const projectRoot = __dirname.replace('/.docz', '');
 const { menuItems } = require(`${projectRoot}/config/menu.json`);
 
 export default {
-  title: 'Cartlog Manual',
-  menu: menuItems
+  title: 'Cartlog Manual'
 }
